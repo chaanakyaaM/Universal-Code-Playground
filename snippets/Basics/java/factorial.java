29f0990fd48021c317dcc1d2ae4609e1744ef51c
insert name <76984162+NotSundarPichai@users.noreply.github.com>
2025-10-07 00:02:41 +0530

    Update factorial.java

--- snippets/Basics/java/factorial.java
+++ snippets/Basics/java/factorial.java
@@ -1,6 +1,16 @@
 # Title: Iterative factorial
 # Topic: Basics
 # Language: java
-# Example: see bottom
 
-// Iterative factorial - placeholder in java
+public class Factorial {
+    static int fact(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " = " + fact(num));
+    }
+}
