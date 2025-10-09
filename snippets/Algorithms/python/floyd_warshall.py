3bd544629efc47fa6f766b97fa13378ac7ee99dc
unknown <chaanakyaam@gmail.com>
2025-10-09 18:26:24 +0530

    changed the value from -1 to inf to represent no edge weight

--- snippets/Algorithms/python/floyd_warshall.py
+++ snippets/Algorithms/python/floyd_warshall.py
@@ -1,7 +1,7 @@
 
 # Metadata Header (MANDATORY)
 # -----------------------------
-# Program Title: Floyd Warshall Algorithm
+# # Program Title: Floyd Warshall Algorithm
 # Author: [Chaanakyaa Milkuri/@chaanakyaaM]
 # Date: 2025-10-08
 #
@@ -17,6 +17,7 @@
 # problem using Floyd Warshall algorithm
 def floydWarshall(graph):
     V = len(graph)
+    INF = float('inf')  # Using infinity to represent no path
 
     # Add all vertices one by one to
     # the set of intermediate vertices.
@@ -31,23 +32,21 @@ def floydWarshall(graph):
 
                 # If vertex k is on the shortest path from
                 # i to j, then update the value of graph[i][j]
-
-                if ((graph[i][j] == -1 or 
-                    graph[i][j] > (graph[i][k] + graph[k][j]))
-                    and (graph[k][j] != -1 and graph[i][k] != -1)):
+                if (graph[i][j] > (graph[i][k] + graph[k][j])):
                     graph[i][j] = graph[i][k] + graph[k][j]
 
 if __name__ == "__main__":
     graph = [
-        [0, 4, -1, 5, -1],
-        [-1, 0, 1, -1, 6],
-        [2, -1, 0, 3, -1],
-        [-1, -1, 1, 0, 2],
-        [1, -1, -1, 4, 0]
+        [0, 4, float('inf'), 5, float('inf')],
+        [float('inf'), 0, 1, float('inf'), 6],
+        [2, float('inf'), 0, 3, float('inf')],
+        [float('inf'), float('inf'), 1, 0, 2],
+        [1, float('inf'), float('inf'), 4, 0]
     ]
     
     floydWarshall(graph)
     for i in range(len(graph)):
         for j in range(len(graph)):
-            print(graph[i][j], end=" ")
+            # If the distance is infinity, print "INF" for clarity
+            print("INF" if graph[i][j] == float('inf') else graph[i][j], end=" ")
         print()
